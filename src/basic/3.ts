ca31e90b5a55683e0e09a16a6d3f9ffd7fc50a59
Valentyn Mostovyi <mostovoy.v@gmail.com>
2024-12-21 18:50:59 +0100

    Finish Homework 1

---
+++ src/basic/3.ts
@@ -0,0 +1,3 @@
+let mixedVariable: string | number;
+
+let choice: 'enable' | 'disable';
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import './style.css';
+
+import './basic/5';
+import './generics/3';
+import './generics/4';
+
